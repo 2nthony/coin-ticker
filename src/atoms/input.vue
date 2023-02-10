c6e691c4604b15ed96ed4d61eba0789b355832cf
2nthony <ant.hu@outlook.com>
2023-02-10 22:29:02 +0800

    themed input

---
+++ src/atoms/input.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event.target as any).value)"
+    class="px-2 py-1 rounded-md"
+    :class="[
+      'bg-neutral-200',
+      'dark:bg-neutral-800',
+      'focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2',
+    ]"
+    spellcheck="false"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
