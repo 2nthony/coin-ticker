60c498dac3a52828f7249dcaa98cf1985f657365
2nthony <ant.hu@outlook.com>
2023-02-11 03:24:51 +0800

    fix: tweak tracking coins

--- src/components/list-tracking-coins.vue
+++ src/components/list-tracking-coins.vue
@@ -4,24 +4,24 @@
       <Checkbox
         :checked="coin.pin"
         @change="(checked) => (coin.pin = checked)"
-        class="w-full inline-flex items-center gap-x-2"
+        class="w-full inline-flex items-center gap-x-2 px-2 py-0.5 rounded hover:bg-neutral-300 dark:hover:bg-neutral-700 bg-opacity-10"
       >
-        <span class="w-full inline-grid grid-cols-7 gap-x-1">
-          <span class="uppercase truncate col-span-1">{{ coin.symbol }}</span>
+        <span class="w-full inline-grid grid-cols-12 gap-x-1">
+          <span class="uppercase truncate col-span-2">{{ coin.symbol }}</span>
 
-          <span class="truncate col-span-3">{{ coin.name }}</span>
+          <span class="truncate col-span-5">{{ coin.name }}</span>
 
           <ListDataSlot :id="coin.id" v-slot="{ data, positive }">
             <span
               :class="[positive ? 'text-emerald-500' : 'text-rose-500']"
-              class="truncate col-span-2"
+              class="truncate col-span-3"
             >
               ${{ currencify(data?.usd) }}
             </span>
 
             <span
               :class="[positive ? 'text-emerald-500' : 'text-rose-500']"
-              class="col-span-1"
+              class="col-span-2"
             >
               {{ positive ? "+" : "-"
               }}{{ Math.abs(data?.usd_24h_change).toFixed(2) }}%
