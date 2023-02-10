39da93f034f1628f40b29b0c8eeb9d30127cc11e
2nthony <ant.hu@outlook.com>
2023-02-10 21:50:43 +0800

    move tick logic to App vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <script setup lang="ts">
-import Greet from "./components/Greet.vue";
+import { watch } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+import { api } from "./api";
+import { useStore } from "./store";
+import { currencify } from "./helpers";
+import { useAsyncState, useIntervalFn } from "@vueuse/core";
 import NavBar from "./components/nav-bar.vue";
+
+const ticktime = 33333;
+const { latestData, trackingCoins, pinCoins } = useStore();
+
+const { execute, isLoading } = useAsyncState(
+  () =>
+    api.price({
+      ids: trackingCoins.value.map((i) => i.id),
+      vs_currencies: "usd",
+      include_24hr_change: true,
+    }),
+  null,
+);
+
+function tick() {
+  execute().then((v) => {
+    if (v) {
+      latestData.value = v;
+    }
+  });
+}
+
+function updateTrayText() {
+  const text = pinCoins.value
+    .map((i) => {
+      const data = (latestData.value as any)[i.id];
+      return `${i.symbol.slice(0, 1).toUpperCase()} ${
+        currencify(data?.usd) ?? "-"
+      }`;
+    })
+    .join("  ");
+
+  invoke("update_tray_text", { text });
+}
+
+const { pause, resume } = useIntervalFn(tick, ticktime);
+watch(isLoading, (v) => {
+  if (v) pause();
+  else resume();
+});
+
+watch(latestData, updateTrayText);
+watch(pinCoins, updateTrayText, { deep: true });
+watch(() => trackingCoins.value.length, tick);
 </script>
 
 <template>
-  <div class="container space-y-4">
+  <div class="space-y-4">
     <NavBar />
-
     <RouterView />
-    <!-- <RouterView #="{ Component }">
-      <Transition>
-        <KeepAlive>
-          <component :is="Component" />
-        </KeepAlive>
-      </Transition>
-    </RouterView> -->
-
-    <Greet />
   </div>
 </template>
