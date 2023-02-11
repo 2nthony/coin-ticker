5e859f0dda3ade6ab936d70e0cd402ebb52a2c05
2nthony <ant.hu@outlook.com>
2023-02-11 21:06:44 +0800

    fix root layout

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ watch(() => trackingCoins.value.length, tick);
 
 <template>
   <NavBar
-    class="bg fixed top-0 left-0 right-0 h-8 px-4 flex items-center rounded-lg"
+    class="bg fixed top-0 left-0 right-0 px-4 flex items-center rounded-lg"
   />
-  <RouterView class="pt-8 overflow-y-scroll h-full" />
+  <RouterView class="overflow-y-scroll h-full" />
 </template>
