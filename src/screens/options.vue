29ee1b66ab7da585cbb598d9bf086c4f2df5f6cf
2nthony <ant.hu@outlook.com>
2023-02-11 22:39:24 +0800

    options screen

--- src/screens/options.vue
+++ src/screens/options.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="px-2">options screen</div>
+  <div class="flex flex-col gap-y-2 px-2">
+    <div class="">
+      <h3>Menubar</h3>
+      <p>
+        <Checkbox v-model:checked="options.menubar.showFullSymbol">
+          Show full symbol
+          <span class="hint">(BTC or B)</span>
+        </Checkbox>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Checkbox from "../atoms/checkbox.vue";
+import { useStore } from "../store";
+
+const { options } = useStore();
+</script>
+
+<style scoped lang="postcss">
+h3 {
+  @apply text-xl font-bold;
+}
+
+.hint {
+  @apply ml-2 text-neutral-500;
+}
+</style>
