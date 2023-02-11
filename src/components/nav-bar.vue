c599865ab1db1010853f59573fd2d14935a2af21
2nthony <ant.hu@outlook.com>
2023-02-11 21:06:39 +0800

    navbar use icon

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,9 +1,20 @@
 <template>
-  <ul class="flex gap-2">
-    <li v-for="{ path, meta } in routes" :key="path">
-      <RouterLink :to="path">{{ meta?.label }}</RouterLink>
-    </li>
-  </ul>
+  <div v-bind="$attrs" class="flex justify-between items-center h-12">
+    <ul class="flex gap-2">
+      <li v-for="{ path, meta } in routes" :key="path">
+        <RouterLink :to="path">
+          <div
+            :title="meta!.label as string"
+            :class="icons[path]"
+            class="text-lg"
+          />
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+
+  <!-- placeholder -->
+  <div class="h-12" />
 </template>
 
 <script setup lang="ts">
@@ -11,4 +22,11 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 const { routes } = router.options;
+
+const icons: Record<string, string> = {
+  "/": "i-fa6-solid-list-check",
+  "/select-coins": "i-fa6-solid-list",
+  "/options": "i-fa6-solid-gear",
+  "/about": "i-fa6-solid-circle-exclamation",
+};
 </script>
